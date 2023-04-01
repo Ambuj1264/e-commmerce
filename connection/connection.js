861c8d7bc8fab6f8bdd28efcc6cd3effde51b62a
AMBUJ SINGH <82210881+Ambuj1264@users.noreply.github.com>
2023-04-01 11:06:04 +0530

    Update connection.js

--- connection/connection.js
+++ connection/connection.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://ambuj:4614@cluster0.ae2f0qd.mongodb.net/e-comm",{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect("mongodb+srv://ambuj:4614@cluster0.j9fakum.mongodb.net/sells)
 .then(()=>console.log("Connected to MongoDB"))
 .catch((err)=>console.log("Could not connect to MongoDB",err))
