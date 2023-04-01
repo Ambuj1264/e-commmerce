c945dc5e6f211a51d438cb10a6980fdc4270d27c
AMBUJ SINGH <82210881+Ambuj1264@users.noreply.github.com>
2023-04-01 17:35:41 +0530

    Update connection.js

--- connection/connection.js
+++ connection/connection.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
 mongoose.set('strictQuery', true);
-mongoose.connect(mongodb+srv://vercel-admin-user:AIjN1hlAynYpoXgL@cluster0.j9fakum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://vercel-admin-user:AIjN1hlAynYpoXgL@cluster0.j9fakum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 .then(()=>console.log("Connected to MongoDB"))
 .catch((err)=>console.log("Could not connect to MongoDB",err))
