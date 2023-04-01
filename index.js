12fda9c1d2329577e4cd43ff54f4b4be94a20fa2
AMBUJ SINGH <82210881+Ambuj1264@users.noreply.github.com>
2023-04-01 09:48:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const express=require("express")
 const app=express();
 const db=require("./connection/connection")
-const port=process.env.PORT || 4500
+const port= || 4500
 const cors=require("cors")
 const router=require("./routes/router")
 const fileUpload = require('express-fileupload');
-const passport = require('passport');
+// const passport = require('passport');
 
 
 
 app.use(cors())
 
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(fileUpload({
     useTempFiles : true,
 }));
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(router)
-
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`)
 }
